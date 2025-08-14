dad2f9d4f9a533a0b4259759619c5c2860c7fde8
TURARY1024 <turary1024@protonmail.com>
2025-08-14 22:50:09 +0800

    2025/08/14 modify main.py

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from flask import Flask
+# from flask import Flask
+# from app import create_app
+#
+# app = create_app()
+#
+#
+# if __name__ == "__main__":
+#     # app.run(debug=True, port=8001, host="0.0.0.0")
+#     app.run(debug=False, use_reloader=False, port=8001, host="0.0.0.0")
+#
+import os
 from app import create_app
 
 app = create_app()
 
-
 if __name__ == "__main__":
-    # app.run(debug=True, port=8001, host="0.0.0.0")
-    app.run(debug=False, use_reloader=False, port=8001, host="0.0.0.0")
-
+    port = int(os.environ.get("PORT", 10000))  # ✅ Render 會自動提供 PORT 環境變數
+    app.run(host="0.0.0.0", port=port)
